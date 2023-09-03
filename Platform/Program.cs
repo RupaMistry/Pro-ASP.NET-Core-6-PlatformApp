fde9e67f64e54a58b5e8e3f32b958128bc8a0568
RupaMistry <mistryrupa7@gmail.com>
2023-09-03 21:04:52 +0400

    Logging Messages in the Program.cs File

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -10,6 +10,22 @@ myNewLogger.LogDebug($"Pipleine configuration started");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
+app.MapGet("/", async (HttpContext context, IConfiguration configuration, IWebHostEnvironment env) =>
+{
+    var defaultSetting = configuration["Logging:LogLevel:Default"];
+    await context.Response.WriteAsync($"The default settings is: {defaultSetting}");
+
+    // You can access the Environment value either by using IConfiguration or by directly through IWebHostEnvironment.
+    //var envConfig = configuration["ASPNETCORE_ENVIRONMENT"];
+    //await context.Response.WriteAsync($"\nEnvironment settings is: {envConfig}");
+    await context.Response.WriteAsync($"\nEnvironment settings is: {env.EnvironmentName}");
+
+    var userSecretKey = configuration["WebService:Id"];
+    var userSecretValue = configuration["WebService:Key"];
+
+    await context.Response.WriteAsync($"\nnThe secret ID is: {userSecretKey}\nThe secret key is: {userSecretValue}");
+});
+
 
 myNewLogger.LogDebug($"Pipleine configuration ended");
 
